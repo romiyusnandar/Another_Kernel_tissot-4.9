af3026311579b489f3bd2753a4d6eb200418aa4b
AnGgIt86 <jarbull86@gmail.com>
2021-06-07 21:33:56 +0700

    Add drivers/soc: Introducing Touchboost driver

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -41,12 +41,36 @@ struct events {
 };
 static struct events events_group;
 static struct task_struct *events_notify_thread;
+static int touchboost = 0;
 
 /**************************sysfs start********************************/
 /*
  * Userspace sends cpu#:min_freq_value to vote for min_freq_value as the new
  * scaling_min. To withdraw its vote it needs to enter cpu#:0
  */
+static int set_touchboost(const char *buf, const struct kernel_param *kp)
+{
+	int val;
+
+ 	if (sscanf(buf, "%d\n", &val) != 1)
+ 		return -EINVAL;
+
+ 	touchboost = val;
+
+	return 0;
+}
+
+ static int get_touchboost(char *buf, const struct kernel_param *kp)
+ {
+ 	return snprintf(buf, PAGE_SIZE, "%d", touchboost);
+ }
+
+static const struct kernel_param_ops param_ops_touchboost = {
+ 	.set = set_touchboost,
+ 	.get = get_touchboost,
+};
+device_param_cb(touchboost, &param_ops_touchboost, NULL, 0644);
+
 static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 {
 	int i, j, ntokens = 0;
@@ -138,7 +162,11 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	struct cpu_status *i_cpu_stats;
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
-	int ret;
+	
+	  const char *reset = "0:0 2:0";
+
+ 	if (touchboost == 0)
+ 		cp = reset;
 
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
@@ -147,7 +175,11 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	if (!(ntokens % 2))
 		return -EINVAL;
 
-	cp = buf;
+	if (touchboost == 0)
+ 		cp = reset;
+ 	else
+ 		cp = buf;
+
 	cpumask_clear(limit_mask);
 	for (i = 0; i < ntokens; i += 2) {
 		if (sscanf(cp, "%u:%u", &cpu, &val) != 2)
@@ -169,17 +201,15 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 		i_cpu_stats = &per_cpu(cpu_stats, i);
 		if (cpufreq_get_policy(&policy, i))
 			continue;
-
-		if (cpu_online(i) && (policy.max != i_cpu_stats->max)) {
-			ret = cpufreq_update_policy(i);
-			if (ret)
-				continue;
-		}
+			
+		if (cpu_online(i) && (policy.max != i_cpu_stats->max))
+			cpufreq_update_policy(i);
+			
 		for_each_cpu(j, policy.related_cpus)
 			cpumask_clear_cpu(j, limit_mask);
 	}
 	put_online_cpus();
-
+	
 	return 0;
 }
 

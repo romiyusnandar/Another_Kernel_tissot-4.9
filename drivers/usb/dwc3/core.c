e2569e8824c51546f718d3b527efdd1858bfe18d
Johan Hovold <johan+linaro@kernel.org>
2022-08-04 17:09:53 +0200

    usb: dwc3: fix PHY disable sequence
    commit d2ac7bef95c9ead307801ccb6cb6dfbeb14247bf upstream.
    
    Generic PHYs must be powered-off before they can be tore down.
    
    Similarly, suspending legacy PHYs after having powered them off makes no
    sense.
    
    Fix the dwc3_core_exit() (e.g. called during suspend) and open-coded
    dwc3_probe() error-path sequences that got this wrong.
    
    Note that this makes dwc3_core_exit() match the dwc3_core_init() error
    path with respect to powering off the PHYs.
    
    Fixes: 03c1fd622f72 ("usb: dwc3: core: add phy cleanup for probe error handling")
    Fixes: c499ff71ff2a ("usb: dwc3: core: re-factor init and exit paths")
    Cc: stable@vger.kernel.org      # 4.8
    Reviewed-by: Andrew Halaney <ahalaney@redhat.com>
    Reviewed-by: Matthias Kaehlcke <mka@chromium.org>
    Reviewed-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>
    Link: https://lore.kernel.org/r/20220804151001.23612-2-johan+linaro@kernel.org
    [ johan: adjust context to 4.9 ]
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -695,11 +695,15 @@ static void dwc3_core_exit(struct dwc3 *dwc)
 {
 	dwc3_event_buffers_cleanup(dwc);
 
-	phy_exit(dwc->usb2_generic_phy);
-	phy_exit(dwc->usb3_generic_phy);
-
+	usb_phy_set_suspend(dwc->usb2_phy, 1);
+	usb_phy_set_suspend(dwc->usb3_phy, 1);
 	phy_power_off(dwc->usb2_generic_phy);
 	phy_power_off(dwc->usb3_generic_phy);
+
+	usb_phy_shutdown(dwc->usb2_phy);
+	usb_phy_shutdown(dwc->usb3_phy);
+	phy_exit(dwc->usb2_generic_phy);
+	phy_exit(dwc->usb3_generic_phy);
 }
 
 /**
@@ -1375,8 +1379,30 @@ static int dwc3_probe(struct platform_device *pdev)
 	pm_runtime_allow(dev);
 	return 0;
 
-err_core_init:
-	dwc3_core_exit_mode(dwc);
+err5:
+	dwc3_event_buffers_cleanup(dwc);
+
+	usb_phy_set_suspend(dwc->usb2_phy, 1);
+	usb_phy_set_suspend(dwc->usb3_phy, 1);
+	phy_power_off(dwc->usb2_generic_phy);
+	phy_power_off(dwc->usb3_generic_phy);
+
+	usb_phy_shutdown(dwc->usb2_phy);
+	usb_phy_shutdown(dwc->usb3_phy);
+	phy_exit(dwc->usb2_generic_phy);
+	phy_exit(dwc->usb3_generic_phy);
+
+	dwc3_ulpi_exit(dwc);
+
+err4:
+	dwc3_free_scratch_buffers(dwc);
+
+err3:
+	dwc3_free_event_buffers(dwc);
+	dwc3_ulpi_exit(dwc);
+
+err2:
+	pm_runtime_allow(&pdev->dev);
 
 err1:
 	destroy_workqueue(dwc->dwc_wq);

13828ccdf52f29682981895fb1ab730e6a91216f
Kakashi <android@building>
2022-08-13 12:14:16 +0700

    driver: added smartassV2 governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -598,6 +598,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CONFIG_DEFAULT_GOVERNOR		(&cpufreq_gov_smartass2)
 #endif
 
 /*********************************************************************

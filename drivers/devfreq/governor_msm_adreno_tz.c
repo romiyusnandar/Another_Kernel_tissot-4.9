d7465ce07812c81966fd0bcdfaea76ecc33a99ad
Kakashi <android@building>
2022-08-18 11:58:52 +0700

    adreno_idler: convert to state notifier

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -20,7 +20,7 @@
 #include <linux/io.h>
 #include <linux/ftrace.h>
 #include <linux/mm.h>
-#include <linux/display_state.h>
+#include <linux/state_notifier.h>
 #include <linux/msm_adreno_devfreq.h>
 #include <asm/cacheflush.h>
 #include <soc/qcom/scm.h>
@@ -74,8 +74,6 @@ static void do_partner_stop_event(struct work_struct *work);
 static void do_partner_suspend_event(struct work_struct *work);
 static void do_partner_resume_event(struct work_struct *work);
 
-/* Display and suspend state booleans */ 
-static bool display_on;
 static bool suspended = false;
 
 static struct workqueue_struct *workqueue;
@@ -472,7 +470,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq)
 	 * Force to use & record as min freq when system has
 	 * entered pm-suspend or screen-off state.
 	 */
-	if (suspended || !display_on) {
+	if (suspended || state_suspended) {
 		*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 1];
 		return 0;
 	}
@@ -714,7 +712,6 @@ static int tz_suspend(struct devfreq *devfreq)
 	unsigned int scm_data[2] = {0, 0};
 
 	__secure_tz_reset_entry2(scm_data, sizeof(scm_data), priv->is_64);
-	display_on = is_display_on();
 	suspended = true;
 
 	priv->bin.total_time = 0;
@@ -766,7 +763,6 @@ static int tz_handler(struct devfreq *devfreq, unsigned int event, void *data)
 	case DEVFREQ_GOV_RESUME:
 		spin_lock(&suspend_lock);
 		suspend_time += suspend_time_ms();
-		display_on = is_display_on();
 		suspended = false;
 		/* Reset the suspend_start when gpu resumes */
 		suspend_start = 0;

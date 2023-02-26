89182c6c37169110df08b5ab9e98868866e07811
RyuujiX <saputradenny712@gmail.com>
2023-02-27 06:02:09 +0700

    treewide: Use Second way for KernelSU
    - Seems KProbes is broken.
    - Based on https://kernelsu.org/guide/how-to-integrate-for-non-gki.html.
    
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1676,6 +1676,11 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
+#ifdef CONFIG_KSU
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+#endif
+
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1687,6 +1692,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 	bool is_su;
+
+#ifdef CONFIG_KSU
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);

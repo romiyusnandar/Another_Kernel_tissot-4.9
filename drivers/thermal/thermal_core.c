81cf35dde52c5e5e846a8195e955d0a9aae1b0e5
Wei Wang <wvw@google.com>
2023-04-02 18:54:00 +0700

    drivers: thermal: step_wise: Handle the temperature stable trend
    It is unnecessary to update disabled thermal zones post suspend and
    sometimes leads error/warning in bad behaved thermal drivers.

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -2625,9 +2625,15 @@ static int thermal_pm_notify(struct notifier_block *nb,
 	case PM_POST_SUSPEND:
 		atomic_set(&in_suspend, 0);
 		list_for_each_entry(tz, &thermal_tz_list, node) {
-			if (tz->ops->is_wakeable &&
-				tz->ops->is_wakeable(tz))
+			tz_mode = THERMAL_DEVICE_ENABLED;
+			if (tz->ops->get_mode)
+				tz->ops->get_mode(tz, &tz_mode);
+
+			if ((tz->ops->is_wakeable &&
+				tz->ops->is_wakeable(tz)) ||
+				tz_mode == THERMAL_DEVICE_DISABLED)
 				continue;
+			
 			thermal_zone_device_init(tz);
 			thermal_zone_device_update(tz,
 						   THERMAL_EVENT_UNSPECIFIED);

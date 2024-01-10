de1ef2e296912dd831249730b9a693a679d7871f
sirnewbies <yusromi04@gmail.com>
2024-01-10 06:27:07 +0000

    Revert "drivers: fs: Add fix for latest kernelsu"
    This reverts commit 5208f1aca44a05734f96b1bc11bd5424a870a2a2.

--- fs/exec.c
+++ fs/exec.c
@@ -1673,11 +1673,6 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
-extern bool ksu_execveat_hook __read_mostly;
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			      void *envp, int *flags);
-extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
-			      void *argv, void *envp, int *flags);
 /*
  * sys_execve() executes a new program.
  */
@@ -1692,11 +1687,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 	bool is_su;
-	
-	if (unlikely(ksu_execveat_hook))
-		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-	else
-		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
